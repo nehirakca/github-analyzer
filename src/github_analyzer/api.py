0b8d59bd3199d191435b937c937fe9e952181694
Nehir Akca <nehirakca9@gmail.com>
2026-08-30 19:41:58 +0300

    refactor: improve GitHub API layer

--- src/github_analyzer/api.py
+++ src/github_analyzer/api.py
@@ -8,51 +8,40 @@ class User(TypedDict):
     following:int
     public_repos:int
 
-def get_user_profile(username:str) -> Optional[User]:
+class Repository(TypedDict):
+    language:Optional[str]
+    stargazers_count:int
+
+def api_request(url:str, params:Optional[dict] = None) -> Optional[dict | list]:
     try:
-        url=f"https://api.github.com/users/{username}"
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, params=params, timeout=10)
         if response.status_code == 200:
             return response.json()
-        elif response.status_code == 404:
-            print("User not found!")
-            return None
-        elif response.status_code == 403:
-            print("Access denied or rate limit exceeded!")
-            return None
         else:
-            print("API error!")
             return None
     except requests.exceptions.RequestException:
         return None
 
-def get_repositories(username:str) -> Optional[list[dict]]:
-    try:
-        repositories=[]
-        page=1
-        url=f"https://api.github.com/users/{username}/repos"
-        while True:
-            params={
-                "page":page,
-                "per_page":100
-            }
-            response=requests.get(url, params=params, timeout=10)
-            if response.status_code == 200:
-                new_repositories = response.json()
-                repositories.extend(new_repositories)
-                if len(new_repositories) < 100:
-                    break
-                else:
-                    page+=1
-            elif response.status_code == 404:
-                print("User not found!")
-                return None
-            elif response.status_code == 403:
-                print("Access denied or rate limit exceeded!")
-                return None
+def get_user_profile(username:str) -> Optional[User]:
+    url=f"https://api.github.com/users/{username}"
+    return api_request(url)
+
+def get_repositories(username:str) -> Optional[list[Repository]]:
+    repositories=[]
+    page=1
+    url=f"https://api.github.com/users/{username}/repos"
+    while True:
+        params={
+            "page":page,
+            "per_page":100
+        }
+        new_repositories = api_request(url, params)
+        if new_repositories is not None:
+            repositories.extend(new_repositories)
+            if len(new_repositories) < 100:
+                break
             else:
-                print("API error!")
-                return None
-        return repositories
-    except requests.exceptions.RequestException:
-        return None
+                page+=1
+        else:
+            return None
+    return repositories
